b8a46bbfa5af7fc15f46c5cd10b3b73fe2f1156c
Moein.MAC <moein.mac@gmail.com>
2023-12-06 20:39:41 +0330

    agent-data

---
+++ components/agent.js/agent-data.js
@@ -0,0 +1,39 @@
+const TData = {
+  vision: 3,
+  metabolism: 0.5,
+  diligence: 3,
+  wealth: 12,
+  color: "#E2791C",
+};
+
+const HWData = {
+  vision: 1,
+  metabolism: 0.3,
+  diligence: 1,
+  wealth: 9,
+  color: "#18D6C6",
+};
+
+const RKData = {
+  vision: 1,
+  metabolism: 0.7,
+  diligence: 4,
+  wealth: 40,
+  color: "#F60609",
+};
+
+const CData = {
+  vision: 2,
+  metabolism: 0.3,
+  diligence: 1,
+  wealth: 10,
+  color: "#F43075",
+};
+
+const RData = {
+  vision: 2,
+  metabolism: 0.4,
+  diligence: 2,
+  wealth: 8,
+  color: "#051417",
+};
