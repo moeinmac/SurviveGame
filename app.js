128a5e31e218756d4336f4098a911bbf5c2ddaba
Moein.MAC <moein.mac@gmail.com>
2023-12-06 20:33:11 +0330

    cookie

--- app.js
+++ app.js
@@ -12,4 +12,7 @@ const GameData =new Array(height / size)
   .fill(0)
   .map(() => Array(width / size).fill({ v: 0, id: null }));
 
-gridBoard(ctx)
+gridBoard(ctx);
+
+const cookiesNumber = 200;
+distributeCookies(ctx);
